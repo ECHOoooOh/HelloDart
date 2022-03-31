ef8d23152e99436b3c013d34c76b4b2a81ae9567
ECHOoooOh <1215202632@qq.com>
2022-03-31 08:27:29 +0000

    update to control

--- 01_dart_start.dart
+++ 01_dart_start.dart
@@ -1,4 +1,4 @@
-void variables(){
+void variables(){//变量
     var name = 'Voyager I';
     var year = 1997;
     var antennaDiameter = 3.7;
@@ -10,6 +10,29 @@ void variables(){
     print(
         'name is $name,\n year is $year,\n antennaDiameter is $antennaDiameter,\n flybyObjects is $flybyObjects,\n image is $image'
     );
+
+    //流程控制语句
+    if ( year >= 2001){
+        print ('21st century\n');
+    }else if( year >= 1901){
+        print ('20th century\n');
+    }
+
+    for(final object in flybyObjects){
+        print(object\n);
+    }
+
+    for(int month = 1; month <= 12; month++){
+        print (month\t);
+    }
+    while(year < 2016){
+        year += 1;
+        print(year\t);
+    }
+}
+
+void controlFlow(){
+    if ( year )
 }
 
 void main(){
