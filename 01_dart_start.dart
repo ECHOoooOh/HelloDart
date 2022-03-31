37d670d9443ab5143642ad630b36b28a18edbc13
ECHOoooOh <1215202632@qq.com>
2022-03-31 09:13:52 +0000

    update to class

--- 01_dart_start.dart
+++ 01_dart_start.dart
@@ -8,7 +8,7 @@ void variables(){
         'tags' : ['saturn'],
         'url' : '//path/to/saturn.jpg'
     };
-    /*print(
+    print(
         'name is $name,\n year is $year,\n antennaDiameter is $antennaDiameter,\n flybyObjects is $flybyObjects,\n image is $image'
     );
 
@@ -29,20 +29,56 @@ void variables(){
     while(year < 2016){
         year += 1;
         print('$year\t');
-    }*/
+    }
 
     //（胖箭头）简写语法
     flybyObjects.where((name) => name.contains('turn')).forEach(print);
     //找到flybyObjects中包含turn的所有变量并将其打印（我猜）
 }
 
-int fibonacci(int n){
+int fibonacci(int n){//斐波那契数列
     if( n == 0 || n == 1) return n;
     return fibonacci(n-1) + fibonacci(n - 2);
 }
 
+class Spacecraft{
+    String name;
+    DateTime? launchDate;
+    //dart 中默认进行空安全检查，？表示不用检查
+
+    int? get launchYear => launchdate?.year;
+
+    Spacecraft(this.name, this.launchDate){
+        //初始化代码
+
+    }
+
+    Spacecraft.unlaunched(String name) : this(name, null);
+
+    //方法
+    void describe(){
+        print('Spacecraft:$name');
+        //？？类型提升对getters不起作用
+        var launchDate = this.launchDate;
+        if (launchDate != null){
+            int years = DateTime.now().difference(launchDate).inDays ~/ 365;
+            print ('Launched: $launchYear ($years years ago)');
+        }else{
+            print('Unlaunched');
+        }
+    }
+    
+}
+
 void main(){
-    variables();
+    //variables();
     var result = fibonacci(20);
     //print('fibonacci(20) is $result');
+    var voyager = Spacecraft('Voyager I',DateTime(1997, 9, 5));
+    voyager.describe();
+
+    var voyager3 = Spacecraft.unlaunched('Voyager III');
+    voyager3.describe();
+
+
 }
