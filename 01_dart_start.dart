d8f420e231b6ddfd42d24062ed9f75b4f53ae72c
ECHOoooOh <1215202632@qq.com>
2022-04-04 13:52:51 +0000

    update 4.4

--- 01_dart_start.dart
+++ 01_dart_start.dart
@@ -1,5 +1,7 @@
+//导入
 import 'dart:async';
 import 'dart:io';
+//变量、异常、流程控制
 void variables()async{
     //变量
     var name = 'Voyager I';
@@ -16,21 +18,21 @@ void variables()async{
 
     //流程控制语句
     if ( year >= 2001){
-        print ('21st century\n');
+        print ('21st century');
     }else if( year >= 1901){
-        print ('20th century\n');
+        print ('20th century');
     }
 
     for(final object in flybyObjects){
-        print('$object\n');
+        print('$object');
     }
 
     for(int month = 1; month <= 12; month++){
-        print ('$month\t');
+        print ('$month');
     }
     while(year < 2016){
         year += 1;
-        print('$year\t');
+        print('$year');
     }
 
     //（胖箭头）简写语法
@@ -53,11 +55,13 @@ void variables()async{
     }
 }
 
+//函数
 int fibonacci(int n){//斐波那契数列
     if( n == 0 || n == 1) return n;
     return fibonacci(n-1) + fibonacci(n - 2);
 }
 
+//类
 class Spacecraft{
     String name;
     DateTime? launchDate;
@@ -107,7 +111,7 @@ class Orbiter extends Spacecraft{
         }
     }
 }
-
+//Mixin
 mixin Piloted{
     int astronauts = 1;
 
@@ -125,14 +129,14 @@ class PilotedCraft extends Spacecraft with Piloted{
     
 }
 void useClass(){
-    //var voyager = Spacecraft('Voyager I',DateTime(1997, 9, 5));
-    //voyager.describe();
+    var voyager = Spacecraft('Voyager I',DateTime(1997, 9, 5));
+    voyager.describe();
 
-    //var voyager3 = Spacecraft.unlaunched('Voyager III');
-    //voyager3.describe();
+    var voyager3 = Spacecraft.unlaunched('Voyager III');
+    voyager3.describe();
 
-    //var voyager4 = Orbiter('Voyager IV',DateTime(2022, 3, 31),1800);
-    //voyager4.describe();
+    var voyager4 = Orbiter('Voyager IV',DateTime(2022, 3, 31),1800);
+    voyager4.describe();
 
     var voyager5 = PilotedCraft('Voyager V',DateTime(2012, 5,17));
     voyager5.describe();
@@ -149,7 +153,7 @@ Future<void> printWithDelay(String message) async {
 void main(){
     variables();
     var result = fibonacci(20);
-    //print('fibonacci(20) is $result');
+    print('fibonacci(20) is $result');
     String message='message';
     printWithDelay(message);
     useClass();
